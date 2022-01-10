76a3318b8dc5e95df22c7f8de7fd87af27c4c7b0
Hikachhu <florian.morin@gmail.com>
2022-01-10 23:29:13 +0100

    log in file, serveur start or not ?

--- prediction-engine/src/main/java/fr/lernejo/prediction/Launcher.java
+++ prediction-engine/src/main/java/fr/lernejo/prediction/Launcher.java
@@ -6,10 +6,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import retrofit2.Retrofit;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 @SpringBootApplication
 public class Launcher {
 
     public static void main(String[] args) {
+        try {
+            PrintWriter writer = new PrintWriter("Inscription.txt", StandardCharsets.UTF_8);
+            writer.println("Démarrage application ");
+            writer.close();
+        }catch (IOException e){
+            System.out.println(e);
+        }
         SpringApplication.run(Launcher.class, args);
     }
 
--- site/src/main/java/fr/lernejo/travelsite/SiteService.java
+++ site/src/main/java/fr/lernejo/travelsite/SiteService.java
@@ -2,13 +2,17 @@ package fr.lernejo.travelsite;
 
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Objects;
 
 @Service
 public record SiteService(PredictionEngineClient predictionEngineClient) {
     public List<TempDate> getTemperature(String country) throws IOException {
+        PrintWriter writer = new PrintWriter("SiteService.txt", StandardCharsets.UTF_8);
+        writer.println("Demande de "+country);
+        writer.close();
         return Objects.requireNonNull(
                 predictionEngineClient.getTemperature(country).execute().body()
                 ).temperatures();

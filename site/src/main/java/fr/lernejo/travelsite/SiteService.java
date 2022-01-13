e1bce12c64942afdf0e52ef63de2a7a8e8a4e983
Hikachhu <florian.morin@gmail.com>
2022-01-13 17:43:11 +0100

    fix size of method

--- site/src/main/java/fr/lernejo/travelsite/SiteService.java
+++ site/src/main/java/fr/lernejo/travelsite/SiteService.java
@@ -15,26 +15,19 @@ public record SiteService(PredictionEngineClient predictionEngineClient) {
         PrintWriter writer = new PrintWriter("SiteService.txt", StandardCharsets.UTF_8);
         writer.println("Demande de "+country);
         writer.close();
-        return
-                predictionEngineClient.getTemperature(country).execute().body()
-                .temperatures();
+        return predictionEngineClient.getTemperature(country).execute().body().temperatures();
     }
     public ArrayList<PotentialDestinations> getCountries(Weather weatherExpectation, int minimumTemperatureDistance, String userCountry) throws IOException {
         String content = new String(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("countries.txt")).readAllBytes(), StandardCharsets.UTF_8);
-        Stream<String> countries = content.lines();
         ArrayList<PotentialDestinations> listDest = new ArrayList<>();
-        for (Iterator<String> it = countries.iterator(); it.hasNext(); ) {
+        for (Iterator<String> it = content.lines().iterator(); it.hasNext(); ) {
             String country = it.next();
-            Call<listTempOfCountry> predictionCall = predictionEngineClient.getTemperature(country);
             try {
-                Response<listTempOfCountry> response = predictionCall.execute();
-                assert response.body() != null;
+                Response<listTempOfCountry> response = predictionEngineClient.getTemperature(country).execute();
                 double moyTemp = (response.body().temperatures().get(0).temperature() + response.body().temperatures().get(0).temperature()) / 2;
                 if (moyTemp >= minimumTemperatureDistance && weatherExpectation.equals(Weather.WARMER))listDest.add(new PotentialDestinations(country, moyTemp));
                 else if (moyTemp <= minimumTemperatureDistance && weatherExpectation.equals(Weather.COLDER))listDest.add(new PotentialDestinations(country, moyTemp));
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+            } catch (Exception ex) {ex.printStackTrace();}
         }
         return listDest;
     }

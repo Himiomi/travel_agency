1e4c1ed6163ba843f32f0e4fd4c61145a53a4e5c
Hikachhu <florian.morin@gmail.com>
2022-01-13 23:14:29 +0100

    change method of add countries from api

--- site/src/main/java/fr/lernejo/travelsite/SiteService.java
+++ site/src/main/java/fr/lernejo/travelsite/SiteService.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 @Service
 public record SiteService(PredictionEngineClient predictionEngineClient) {
-    public ArrayList<PotentialDestinations> potentialCountries(Weather meteo, int minTemp) throws IOException {
+    public ArrayList<PotentialDestinations> potentialCountries(Weather meteo, int minTemp, String currentCountry) throws IOException {
         String content = new String(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("countries.txt")).readAllBytes(), StandardCharsets.UTF_8);
         ArrayList<PotentialDestinations> listDest = new ArrayList<>();
         for (Iterator<String> it = content.lines().iterator(); it.hasNext(); ) {
@@ -18,8 +18,8 @@ public record SiteService(PredictionEngineClient predictionEngineClient) {
                 Response<listTempOfCountry> response = predictionEngineClient.getTemperature(country).execute();
                 assert response.body() != null;
                 double moyTemp = (response.body().temperatures().get(0).temperature() + response.body().temperatures().get(0).temperature()) / 2;
-                if (moyTemp > minTemp && meteo.equals(Weather.WARMER))listDest.add(new PotentialDestinations(country, moyTemp));
-                else if (moyTemp < minTemp && meteo.equals(Weather.COLDER))listDest.add(new PotentialDestinations(country, moyTemp));
+                if (currentCountry.equals(response.body().country())&& moyTemp > minTemp && meteo.equals(Weather.WARMER))listDest.add(new PotentialDestinations(country, moyTemp));
+                else if (currentCountry.equals(response.body().country())&&moyTemp < minTemp && meteo.equals(Weather.COLDER))listDest.add(new PotentialDestinations(country, moyTemp));
             } catch (Exception ex) {ex.printStackTrace();}
         }
         return listDest;
